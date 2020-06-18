bde7703a53124739d453ee7d3cc083a658ee7122
SE Zeltmann <steven.zeltmann@lbl.gov>
2020-06-17 17:31:03 -0700

    fix most but seemingly not all vector maths errors

--- py4DSTEM/simulate/kinematic.py
+++ py4DSTEM/simulate/kinematic.py
@@ -7,12 +7,14 @@ from ..process.rdf import single_atom_scatter
 from ..process.utils import electron_wavelength_angstrom
 from ..file.datastructure import PointList, PointListArray
 
+from pdb import set_trace
+
 
 class KinematicLibrary:
     """
-	Kinematic performs kinematic (single scattering) diffraction simulations 
-	to construct a library of simulated PointLists with associated orientations, used for classifying.
-	"""
+    Kinematic performs kinematic (single scattering) diffraction simulations 
+    to construct a library of simulated PointLists with associated orientations, used for classifying.
+    """
 
     def __init__(
         self,
@@ -27,28 +29,28 @@ class KinematicLibrary:
         **kwargs,
     ):
         """
-		Create a Kinematic simulation object. 
+        Create a Kinematic simulation object. 
 
-		Accepts:
-		structure		a pymatgen Structure object for the material
-						or a string containing the Materials Project ID for the 
-						structure (requires API key in config file, see:
-						https://pymatgen.org/usage.html#setting-the-pmg-mapi-key-in-the-config-file
+        Accepts:
+        structure       a pymatgen Structure object for the material
+                        or a string containing the Materials Project ID for the 
+                        structure (requires API key in config file, see:
+                        https://pymatgen.org/usage.html#setting-the-pmg-mapi-key-in-the-config-file
 
-		max_index		maximum hkl indices to compute structure factors
+        max_index       maximum hkl indices to compute structure factors
 
-		poles			numpy array (n,3) containing [h,k,l] indices for the n
-						orientations required in the simulation.
+        poles           numpy array (n,3) containing [h,k,l] indices for the n
+                        orientations required in the simulation.
 
-		voltage			electron kinetic energy in Volts
+        voltage         electron kinetic energy in Volts
 
-		tol_zone		cutoff for discarding reciprocal lattice points that
-						do not exactly satisfy Weiss zone law	
+        tol_zone        cutoff for discarding reciprocal lattice points that
+                        do not exactly satisfy Weiss zone law   
 
-		tol_int			cutoff for excluding very weak structure factors
+        tol_int         cutoff for excluding very weak structure factors
 
-		thickness		sample thickness in Å
-		"""
+        thickness       sample thickness in Å
+        """
 
         if isinstance(structure, str):
             structure = mg.get_structure_from_mp(structure)
@@ -77,20 +79,9 @@ class KinematicLibrary:
         self.tol_zone = tol_zone
         self.tol_int = tol_int
 
-        self.a = self.structure.lattice.abc[0]
-        self.N = thickness // self.a
+        self.thickness = thickness
 
         self.λ = electron_wavelength_angstrom(voltage)
-
-        # rotation matrix to get a nonparallel vector for later calculations
-        # rotates by a small amount about each axis
-        c = np.cos(0.1 / 180 * np.pi)
-        s = np.sin(0.1 / 180 * np.pi)
-        self.R = (
-            np.array([[1, 0, 0], [0, c, -s], [0, s, c]])
-            @ np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
-            @ np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-        )
 
         # ------- run computations --------
         self._compute_structure_factors()
@@ -105,9 +96,13 @@ class KinematicLibrary:
         else:
             self.poles = poles
 
+        # generate the library
         self._run_simulation()
 
     def explore_library(self):
+        """
+        Open an ipython interactive widget to visulaize the generated diffraction patterns
+        """
         from ipywidgets import interactive
         from IPython.display import display
         import matplotlib.pyplot as plt
@@ -122,6 +117,8 @@ class KinematicLibrary:
             plt.title(
                 f"g = [{pl.tags['pole'][0]:.0f},{pl.tags['pole'][1]:.0f},{pl.tags['pole'][2]:.0f}]"
             )
+            plt.xlabel("$q_x [\AA]$")
+            plt.ylabel("$q_y [\AA]$")
             for i in range(pl.length):
                 plt.text(
                     pl.data["qx"][i] + 0.008,
@@ -156,27 +153,32 @@ class KinematicLibrary:
         self.pattern_library = pla
 
     def _generate_pattern(self, uvw: np.array, pointlist: Optional[PointList] = None):
-        # get the diffraction intensities for each reflection
-        pattern = self._get_diffraction_intensities(uvw)
+        # get the diffraction intensities for each reflection (hklI)
+        pattern = self._get_diffraction_intensities(uvw, t=self.thickness)
+
+        # extract the points in (x*,y*,z*) basis
         q = pattern[:, :3] * self.recip_lat
 
+        # unit vector along the foil normal
+        k = (uvw * self.recip_lat) / np.linalg.norm(uvw * self.recip_lat)
+
         # project reflections onto the plane defined by uvw
-        proj_points = q - ((q @ uvw) / np.sqrt(np.sum(uvw ** 2)))[:, np.newaxis]
+        proj_points = q - ((q @ k) / np.sqrt(np.sum(k ** 2)))[:, np.newaxis]
 
         # generate two basis vectors in the plane based on the following scheme:
         # (vectors are compared by checking their angle is below some threshold)
         # if uvw != a*, qx <- a* projected onto the uvw plane
-        # 	else: qx <- uvw ⨉ c* projected onto the uvw plane
+        #   else: qx <- uvw ⨉ c* projected onto the uvw plane
 
-        if vector_angle(uvw, np.array([1.0, 0.0, 0.0], dtype=uvw.dtype)) > 1e-6:
-            x = np.array([1.0, 0.0, 0.0]) - (uvw * (np.array([1.0, 0.0, 0.0]) @ uvw))
+        if vector_angle(k, np.array([1.0, 0.0, 0.0], dtype=k.dtype)) > 1e-6:
+            x = np.array([1.0, 0.0, 0.0]) - (k * (np.array([1.0, 0.0, 0.0]) @ k))
         else:
-            x = np.array([0.0, 0.0, 1.0]) - (uvw * (np.array([0.0, 0.0, 1.0]) @ uvw))
+            x = np.array([0.0, 0.0, 1.0]) - (k * (np.array([0.0, 0.0, 1.0]) @ k))
 
         x /= np.linalg.norm(x)
 
         # y is mutually perpendicular to x and uvw
-        y = np.cross(uvw, x)
+        y = np.cross(k, x)
         y /= np.linalg.norm(y)
 
         # find the projections along these bases to get the diffraction spots
@@ -246,48 +248,45 @@ class KinematicLibrary:
         self.hklI = hklI
         self.hklF = hklF
 
-    def _get_diffraction_intensities(
-        self, uvw: np.ndarray, a: Optional[float] = None, N: Optional[int] = None
-    ) -> np.ndarray:
+    def _get_diffraction_intensities(self, uvw: np.ndarray, t: float) -> np.ndarray:
         # apply zone law to find reciprocal lattice points that may be excited
         # and scale structure factor by shape factor using excitation error
         pattern = np.zeros((1, 4))
 
         hklI = self.hklI
 
-        # find unit vector along the zone
-        uvw_0 = uvw / np.sqrt(np.sum(uvw ** 2))
+        # find unit vector along the zone axis in (x*,y*,z*) basis
+        uvw_0 = (uvw * self.recip_lat) / np.sqrt(np.sum((uvw * self.recip_lat) ** 2))
 
-        k0 = uvw_0 / self.λ
+        k0 = uvw_0 / -self.λ  # incedent wavevector in (x*,y*,z*) basis
 
         for i in range(hklI.shape[0]):
-            P = uvw_0 @ hklI[i, :3]
-            if (np.abs(P) < self.tol_zone) and (hklI[i, 3] > self.tol_int):
+            WZL = uvw @ hklI[i, :3]  # evaluate the Weiss zone law: [uvw] • [hkl]
+            # check if zone law is approximately satisfied, and |F|^2 is above a threshold
+            if (np.abs(WZL) < self.tol_zone) and (hklI[i, 3] > self.tol_int):
                 # set_trace()
-                refl = hklI[i, :].copy()
+                refl = hklI[i, :].copy()  # copy of [ h, k, l, |F|^2 ]
+
+                g = refl[:3] * self.recip_lat  # g in (x*,y*,z*) basis
 
                 # excitation error from de Graef eq. 2.89
-                g = k0 + (refl[:3] * self.recip_lat)
-                exc = (
-                    (-1 * (refl[:3] * self.recip_lat))
-                    @ (2 * k0 + (refl[:3] * self.recip_lat))
-                    / (2 * np.linalg.norm(g) * vector_angle(g, uvw_0))
+                exc = (-1 * g @ ((2 * k0) + g)) / (
+                    2 * np.linalg.norm(k0 + g) * np.cos(vector_angle(k0 + g, uvw_0))
                 )
 
-                # old approximate excitation error
-                # exc = (1 / self.λ) - np.linalg.norm(g)
-
-                refl[3] *= self._shape_factor(exc, self.a, self.N)
+                refl[3] *= self._shape_factor(
+                    np.abs(exc), t
+                )  # damp by shape factor D^2
                 if not np.isnan(refl[3]):
-                    pattern = np.vstack((pattern, refl))
+                    pattern = np.vstack((pattern, refl[np.newaxis, :]))
 
         maxInt = np.nanmax(pattern[:, 3])
-        pattern[0, 3] = maxInt
+        # pattern[0, 3] = maxInt
         pattern[:, 3] /= maxInt
 
         return pattern
 
-    def _generate_transmission(
+    def _get_diffraction_amplitudes(
         self, uvw, a: Optional[float] = None, N: Optional[int] = None
     ) -> np.ndarray:
         # this is the same as _get_diffraction_intensities but returns the complex
@@ -326,14 +325,14 @@ class KinematicLibrary:
         # thick slab and applying it iteratively, along with a propagation operator
         pass
 
-    def _shape_factor(self, s: float, a: float, N: int) -> float:
+    def _shape_factor(self, s: float, t: float) -> float:
         """
-		find SS*(s) for excitation error s, unit cell size a, number atoms N
-		"""
+        find SS*(s) for excitation error s, unit cell size a, number atoms N
+        """
         if np.isclose(s, 0, atol=1e-10):
-            return N ** 2
+            return 1
         else:
-            return np.sin(np.pi * s * a * N) ** 2 / np.sin(np.pi * s * a) ** 2
+            return (np.sin(np.pi * s * t) / (np.pi * s * t)) ** 2
 
     def _cubic_poles(self, n: int) -> np.ndarray:
         print("Using cubic symmetric poles...", flush=True)
@@ -368,14 +367,14 @@ def vector_angle(a: np.ndarray, b: np.ndarray) -> float:
 # fmt: off
 # a dictionary for converting element names into Z
 els = ('H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si',\
-	   'P','S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni',\
-	   'Cu','Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb',\
-	   'Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe',\
-	   'Cs','Ba','La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho',\
-	   'Er','Tm','Yb','Lu','Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg','Tl',\
-	   'Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th','Pa','U','Np','Pu','Am',\
-	   'Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg','Bh','Hs','Mt',\
-	   'Ds','Rg','Cn','Nh','Fl','Mc','Lv','Ts','Og','Uue','Ubn','Ubn')
+       'P','S','Cl','Ar','K','Ca','Sc','Ti','V','Cr','Mn','Fe','Co','Ni',\
+       'Cu','Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb',\
+       'Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I','Xe',\
+       'Cs','Ba','La','Ce','Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho',\
+       'Er','Tm','Yb','Lu','Hf','Ta','W','Re','Os','Ir','Pt','Au','Hg','Tl',\
+       'Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th','Pa','U','Np','Pu','Am',\
+       'Cm','Bk','Cf','Es','Fm','Md','No','Lr','Rf','Db','Sg','Bh','Hs','Mt',\
+       'Ds','Rg','Cn','Nh','Fl','Mc','Lv','Ts','Og','Uue','Ubn','Ubn')
 
 elements = {els[i]: i+1 for i in range(len(els)) }
 # fmt: on
