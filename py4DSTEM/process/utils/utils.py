a5d041ef920c29fcaaed25ba7b3eae6728c4b57d
SE Zeltmann <steven.zeltmann@lbl.gov>
2020-06-23 17:07:11 -0700

    add absolute thresh to bragg disk finding

--- py4DSTEM/process/utils/utils.py
+++ py4DSTEM/process/utils/utils.py
@@ -9,8 +9,10 @@ import math as ma
 try:
     from IPython.display import clear_output
 except ImportError:
+
     def clear_output(wait=True):
         pass
+
 
 def electron_wavelength_angstrom(E_eV):
     m = 9.109383 * 10 ** -31
@@ -18,7 +20,12 @@ def electron_wavelength_angstrom(E_eV):
     c = 299792458
     h = 6.62607 * 10 ** -34
 
-    lam = h / ma.sqrt(2 * m * e * E_eV) / ma.sqrt(1 + e * E_eV / 2 / m / c ** 2) * 10 ** 10
+    lam = (
+        h
+        / ma.sqrt(2 * m * e * E_eV)
+        / ma.sqrt(1 + e * E_eV / 2 / m / c ** 2)
+        * 10 ** 10
+    )
     return lam
 
 
@@ -34,7 +41,7 @@ def sector_mask(shape, centre, radius, angle_range=(0, 360)):
     :return:
     """
 
-    x, y = np.ogrid[:shape[0], :shape[1]]
+    x, y = np.ogrid[: shape[0], : shape[1]]
     cx, cy = centre
     tmin, tmax = np.deg2rad(angle_range)
 
@@ -47,7 +54,7 @@ def sector_mask(shape, centre, radius, angle_range=(0, 360)):
     theta = np.arctan2(x - cx, y - cy) - tmin
 
     # wrap angles between 0 and 2*pi
-    theta %= (2 * np.pi)
+    theta %= 2 * np.pi
 
     # circular mask
     circmask = r2 <= radius * radius
@@ -82,9 +89,9 @@ def get_qx_qy_1d(M, dx=[1, 1], fft_shifted=False):
 def make_Fourier_coords2D(Nx, Ny, pixelSize=1):
     """
     Generates Fourier coordinates for a (Nx,Ny)-shaped 2D array.
-	Specifying the pixelSize argument sets a unit size.
-	"""
-    if hasattr(pixelSize, '__len__'):
+    Specifying the pixelSize argument sets a unit size.
+    """
+    if hasattr(pixelSize, "__len__"):
         assert len(pixelSize) == 2, "pixelSize must either be a scalar or have length 2"
         pixelSize_x = pixelSize[0]
         pixelSize_y = pixelSize[1]
@@ -100,18 +107,18 @@ def make_Fourier_coords2D(Nx, Ny, pixelSize=1):
 
 def get_shift(ar1, ar2, corrPower=1):
     """
-	Determine the relative shift between a pair of identical arrays, or the shift giving
-	best overlap.
+    Determine the relative shift between a pair of identical arrays, or the shift giving
+    best overlap.
 
-	Shift determination uses the brightest pixel in the cross correlation, and is thus limited to
+    Shift determination uses the brightest pixel in the cross correlation, and is thus limited to
     pixel resolution. corrPower specifies the cross correlation power, with 1 corresponding to a
     cross correlation and 0 a phase correlation.
 
-	Inputs:
-		ar1,ar2     2D ndarrays
+    Inputs:
+        ar1,ar2     2D ndarrays
         corrPower   float between 0 and 1, inclusive. 1=cross correlation, 0=phase correlation
-	Outputs:
-		shiftx,shifty - relative image shift, in pixels
+    Outputs:
+        shiftx,shifty - relative image shift, in pixels
     """
     cc = get_cross_correlation(ar1, ar2, corrPower)
     xshift, yshift = np.unravel_index(np.argmax(cc), ar1.shape)
@@ -120,8 +127,8 @@ def get_shift(ar1, ar2, corrPower=1):
 
 def get_shifted_ar(ar, xshift, yshift):
     """
-	Shifts array ar by the shift vector (xshift,yshift), using the Fourier shift theorem (i.e.
-	with sinc interpolation).
+    Shifts array ar by the shift vector (xshift,yshift), using the Fourier shift theorem (i.e.
+    with sinc interpolation).
     """
     nx, ny = np.shape(ar)
     qx, qy = make_Fourier_coords2D(nx, ny, 1)
@@ -171,14 +178,29 @@ def get_maximal_points(ar):
     For 2D array ar, returns an array of bools of the same shape which is True for all entries with
     values larger than all 8 of their nearest neighbors.
     """
-    return (ar > np.roll(ar, (-1, 0), axis=(0, 1))) & (ar > np.roll(ar, (1, 0), axis=(0, 1))) & \
-           (ar > np.roll(ar, (0, -1), axis=(0, 1))) & (ar > np.roll(ar, (0, 1), axis=(0, 1))) & \
-           (ar > np.roll(ar, (-1, -1), axis=(0, 1))) & (ar > np.roll(ar, (-1, 1), axis=(0, 1))) & \
-           (ar > np.roll(ar, (1, -1), axis=(0, 1))) & (ar > np.roll(ar, (1, 1), axis=(0, 1)))
+    return (
+        (ar > np.roll(ar, (-1, 0), axis=(0, 1)))
+        & (ar > np.roll(ar, (1, 0), axis=(0, 1)))
+        & (ar > np.roll(ar, (0, -1), axis=(0, 1)))
+        & (ar > np.roll(ar, (0, 1), axis=(0, 1)))
+        & (ar > np.roll(ar, (-1, -1), axis=(0, 1)))
+        & (ar > np.roll(ar, (-1, 1), axis=(0, 1)))
+        & (ar > np.roll(ar, (1, -1), axis=(0, 1)))
+        & (ar > np.roll(ar, (1, 1), axis=(0, 1)))
+    )
 
 
-def get_maxima_2D(ar, sigma=0, edgeBoundary=0, minSpacing=0, minRelativeIntensity=0,
-                  relativeToPeak=0, maxNumPeaks=0, subpixel=True):
+def get_maxima_2D(
+    ar,
+    sigma=0,
+    edgeBoundary=0,
+    minSpacing=0,
+    minRelativeIntensity=0,
+    minAbsoluteIntensity=0,
+    relativeToPeak=0,
+    maxNumPeaks=0,
+    subpixel=True,
+):
     """
     Finds the indices where the 2D array ar is a local maximum.
     Optional parameters allow blurring of the array and filtering of the output;
@@ -221,12 +243,12 @@ def get_maxima_2D(ar, sigma=0, edgeBoundary=0, minSpacing=0, minRelativeIntensit
 
     # Get indices, sorted by intensity
     maxima_x, maxima_y = np.nonzero(maxima_bool)
-    dtype = np.dtype([('x', float), ('y', float), ('intensity', float)])
+    dtype = np.dtype([("x", float), ("y", float), ("intensity", float)])
     maxima = np.zeros(len(maxima_x), dtype=dtype)
-    maxima['x'] = maxima_x
-    maxima['y'] = maxima_y
-    maxima['intensity'] = ar[maxima_x, maxima_y]
-    maxima = np.sort(maxima, order='intensity')[::-1]
+    maxima["x"] = maxima_x
+    maxima["y"] = maxima_y
+    maxima["intensity"] = ar[maxima_x, maxima_y]
+    maxima = np.sort(maxima, order="intensity")[::-1]
 
     if len(maxima) > 0:
         # Remove maxima which are too close
@@ -234,16 +256,25 @@ def get_maxima_2D(ar, sigma=0, edgeBoundary=0, minSpacing=0, minRelativeIntensit
             deletemask = np.zeros(len(maxima), dtype=bool)
             for i in range(len(maxima)):
                 if deletemask[i] == False:
-                    tooClose = ((maxima['x'] - maxima['x'][i]) ** 2 + \
-                                (maxima['y'] - maxima['y'][i]) ** 2) < minSpacing ** 2
-                    tooClose[:i + 1] = False
+                    tooClose = (
+                        (maxima["x"] - maxima["x"][i]) ** 2
+                        + (maxima["y"] - maxima["y"][i]) ** 2
+                    ) < minSpacing ** 2
+                    tooClose[: i + 1] = False
                     deletemask[tooClose] = True
             maxima = np.delete(maxima, np.nonzero(deletemask)[0])
 
-        # Remove maxima which are too dim
-        if (minRelativeIntensity > 0) & (len(maxima) > relativeToPeak):
+        # Remove maxima which are too dim relative to the brightest spot
+        if (minRelativeIntensity > 0.) & (len(maxima) > relativeToPeak):
             assert isinstance(relativeToPeak, (int, np.integer))
-            deletemask = maxima['intensity'] / maxima['intensity'][relativeToPeak] < minRelativeIntensity
+            deletemask = (
+                maxima["intensity"] / maxima["intensity"][relativeToPeak]
+                < minRelativeIntensity
+            )
+            maxima = np.delete(maxima, np.nonzero(deletemask)[0])
+
+        if (minAbsoluteIntensity > 0.):
+            deletemask = ( maxima['intensity'] < minAbsoluteIntensity)
             maxima = np.delete(maxima, np.nonzero(deletemask)[0])
 
         # Remove maxima in excess of maxNumPeaks
@@ -255,19 +286,21 @@ def get_maxima_2D(ar, sigma=0, edgeBoundary=0, minSpacing=0, minRelativeIntensit
         # Subpixel fitting - fit 1D parabolas in x and y to 3 points (maximum, +/- 1 pixel)
         if subpixel is True:
             for i in range(len(maxima)):
-                Ix1_ = ar[int(maxima['x'][i]) - 1, int(maxima['y'][i])]
-                Ix0 = ar[int(maxima['x'][i]), int(maxima['y'][i])]
-                Ix1 = ar[int(maxima['x'][i]) + 1, int(maxima['y'][i])]
-                Iy1_ = ar[int(maxima['x'][i]), int(maxima['y'][i]) - 1]
-                Iy0 = ar[int(maxima['x'][i]), int(maxima['y'][i])]
-                Iy1 = ar[int(maxima['x'][i]), int(maxima['y'][i]) + 1]
+                Ix1_ = ar[int(maxima["x"][i]) - 1, int(maxima["y"][i])]
+                Ix0 = ar[int(maxima["x"][i]), int(maxima["y"][i])]
+                Ix1 = ar[int(maxima["x"][i]) + 1, int(maxima["y"][i])]
+                Iy1_ = ar[int(maxima["x"][i]), int(maxima["y"][i]) - 1]
+                Iy0 = ar[int(maxima["x"][i]), int(maxima["y"][i])]
+                Iy1 = ar[int(maxima["x"][i]), int(maxima["y"][i]) + 1]
                 deltax = (Ix1 - Ix1_) / (4 * Ix0 - 2 * Ix1 - 2 * Ix1_)
                 deltay = (Iy1 - Iy1_) / (4 * Iy0 - 2 * Iy1 - 2 * Iy1_)
-                maxima['x'][i] += deltax
-                maxima['y'][i] += deltay
-                maxima['intensity'][i] = linear_interpolation_2D(ar, maxima['x'][i], maxima['y'][i])
+                maxima["x"][i] += deltax
+                maxima["y"][i] += deltay
+                maxima["intensity"][i] = linear_interpolation_2D(
+                    ar, maxima["x"][i], maxima["y"][i]
+                )
 
-    return maxima['x'], maxima['y'], maxima['intensity']
+    return maxima["x"], maxima["y"], maxima["intensity"]
 
 
 def get_maxima_1D(ar, sigma=0, minSpacing=0, minRelativeIntensity=0, relativeToPeak=0):
@@ -288,7 +321,9 @@ def get_maxima_1D(ar, sigma=0, minSpacing=0, minRelativeIntensity=0, relativeToP
         An array of indices where ar is a local maximum, sorted by intensity.
     """
     assert len(ar.shape) == 1, "ar must be 1D"
-    assert isinstance(relativeToPeak, (int, np.integer)), "relativeToPeak must be an int"
+    assert isinstance(
+        relativeToPeak, (int, np.integer)
+    ), "relativeToPeak must be an int"
     if sigma > 0:
         ar = gaussian_filter(ar, sigma)
 
@@ -298,7 +333,9 @@ def get_maxima_1D(ar, sigma=0, minSpacing=0, minRelativeIntensity=0, relativeToP
     intensity = ar[maxima_bool]
 
     # Sort by intensity
-    temp_ar = np.array([(x, inten) for inten, x in sorted(zip(intensity, x), reverse=True)])
+    temp_ar = np.array(
+        [(x, inten) for inten, x in sorted(zip(intensity, x), reverse=True)]
+    )
     x, intensity = temp_ar[:, 0], temp_ar[:, 1]
 
     # Remove points which are too close
@@ -307,7 +344,7 @@ def get_maxima_1D(ar, sigma=0, minSpacing=0, minRelativeIntensity=0, relativeToP
         for i in range(len(x)):
             if not deletemask[i]:
                 delete = np.abs(x[i] - x) < minSpacing
-                delete[:i + 1] = False
+                delete[: i + 1] = False
                 deletemask = deletemask | delete
         x = np.delete(x, deletemask.nonzero()[0])
         intensity = np.delete(intensity, deletemask.nonzero()[0])
@@ -339,8 +376,12 @@ def linear_interpolation_2D(ar, x, y):
     y0, y1 = int(np.floor(y)), int(np.ceil(y))
     dx = x - x0
     dy = y - y0
-    return (1 - dx) * (1 - dy) * ar[x0, y0] + (1 - dx) * dy * ar[x0, y1] + dx * (1 - dy) * ar[x1, y0] + dx * dy * ar[
-        x1, y1]
+    return (
+        (1 - dx) * (1 - dy) * ar[x0, y0]
+        + (1 - dx) * dy * ar[x0, y1]
+        + dx * (1 - dy) * ar[x1, y0]
+        + dx * dy * ar[x1, y1]
+    )
 
 
 def add_to_2D_array_from_floats(ar, x, y, I):
@@ -361,8 +402,9 @@ def add_to_2D_array_from_floats(ar, x, y, I):
     return ar
 
 
-def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1,
-                       length=100, fill='*'):
+def print_progress_bar(
+    iteration, total, prefix="", suffix="", decimals=1, length=100, fill="*"
+):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -376,9 +418,9 @@ def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1,
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
+    bar = fill * filledLength + "-" * (length - filledLength)
     clear_output(wait=True)
-    print('\r{} |{}| {}% {}\r'.format(prefix, bar, percent, suffix))
+    print("\r{} |{}| {}% {}\r".format(prefix, bar, percent, suffix))
     # Print New Line on Complete
     if iteration == total:
         print()
@@ -408,7 +450,7 @@ def bin2D(array, factor, dtype=np.float64):
     # Collect pixel sums into new bins
     for ix in range(factor):
         for iy in range(factor):
-            binned_ar += array[0 + ix:xx + ix:factor, 0 + iy:yy + iy:factor]
+            binned_ar += array[0 + ix : xx + ix : factor, 0 + iy : yy + iy : factor]
     return binned_ar
 
 
@@ -435,7 +477,9 @@ def get_voronoi_vertices(voronoi, nx, ny, dist=10):
         vertex_list (list of ndarrays of shape (N,2)) the (x,y) coords of the vertices of each
                     voronoi region
     """
-    assert isinstance(voronoi, Voronoi), "voronoi must be a scipy.spatial.Voronoi instance"
+    assert isinstance(
+        voronoi, Voronoi
+    ), "voronoi must be a scipy.spatial.Voronoi instance"
 
     vertex_list = []
 
@@ -446,9 +490,9 @@ def get_voronoi_vertices(voronoi, nx, ny, dist=10):
     for i in range(len(voronoi.ridge_vertices)):
         ridge = voronoi.ridge_vertices[i]
         if -1 in ridge:
-            edgeridge_vertices_and_points.append([max(ridge),
-                                                  voronoi.ridge_points[i, 0],
-                                                  voronoi.ridge_points[i, 1]])
+            edgeridge_vertices_and_points.append(
+                [max(ridge), voronoi.ridge_points[i, 0], voronoi.ridge_points[i, 1]]
+            )
     edgeridge_vertices_and_points = np.array(edgeridge_vertices_and_points)
 
     # Loop over all regions
@@ -466,8 +510,12 @@ def get_voronoi_vertices(voronoi, nx, ny, dist=10):
                 # For unknown vertices, get the first vertex it connects to,
                 # and the two voronoi points that this ridge divides
                 index_prev = vertex_indices[(i - 1) % len(vertex_indices)]
-                edgeridge_index = int(np.argwhere(edgeridge_vertices_and_points[:, 0] == index_prev))
-                index_vert, region0, region1 = edgeridge_vertices_and_points[edgeridge_index, :]
+                edgeridge_index = int(
+                    np.argwhere(edgeridge_vertices_and_points[:, 0] == index_prev)
+                )
+                index_vert, region0, region1 = edgeridge_vertices_and_points[
+                    edgeridge_index, :
+                ]
                 x, y = voronoi.vertices[index_vert]
                 # Only add new points for unknown vertices if the known index it connects to
                 # is inside the frame.  Add points by finding the line segment starting at
@@ -491,8 +539,12 @@ def get_voronoi_vertices(voronoi, nx, ny, dist=10):
 
                 # Repeat for the second vertec the unknown vertex connects to
                 index_next = vertex_indices[(i + 1) % len(vertex_indices)]
-                edgeridge_index = int(np.argwhere(edgeridge_vertices_and_points[:, 0] == index_next))
-                index_vert, region0, region1 = edgeridge_vertices_and_points[edgeridge_index, :]
+                edgeridge_index = int(
+                    np.argwhere(edgeridge_vertices_and_points[:, 0] == index_next)
+                )
+                index_vert, region0, region1 = edgeridge_vertices_and_points[
+                    edgeridge_index, :
+                ]
                 x, y = voronoi.vertices[index_vert]
                 if (x > 0) and (x < nx) and (y > 0) and (y < ny):
                     x_r0, y_r0 = voronoi.points[region0]
@@ -519,23 +571,28 @@ def get_voronoi_vertices(voronoi, nx, ny, dist=10):
 
     return vertex_list
 
-def ewpc_lambda(Q_Nx,Q_Ny):
-    '''
+
+def ewpc_lambda(Q_Nx, Q_Ny):
+    """
     Returns a lambda for comuting the exit wave power cepstrum of a diffraction pattern
     using a Hanning window. This can be passed as the filter_function in the Bragg disk
     detection functions (with the probe an array of ones) to find the lattice vectors 
     by the EWPC method (but be careful as the lengths are now in realspace units!)
     See https://arxiv.org/abs/1911.00984
-    '''
-    h = np.hanning(Q_Nx)[:,np.newaxis] * np.hanning(Q_Ny)[np.newaxis,:]
-    return lambda x: np.abs(np.fft.fftshift(np.fft.fft2(h*np.log(np.maximum(x,0.01)))))**2
+    """
+    h = np.hanning(Q_Nx)[:, np.newaxis] * np.hanning(Q_Ny)[np.newaxis, :]
+    return (
+        lambda x: np.abs(np.fft.fftshift(np.fft.fft2(h * np.log(np.maximum(x, 0.01)))))
+        ** 2
+    )
+
 
 # Deprecated make_Fourier_coords functions - these are identical to np.fft.fftfreq
 
 # def make_Fourier_coords1D(N, pixelSize=1):
 #    """
 #    Generates Fourier coordinates for a 1D array of length N.
-#	Specifying the pixelSize argument sets a unit size.
+#   Specifying the pixelSize argument sets a unit size.
 #    """
 #    if N%2 == 0:
 #        q = np.roll( np.arange(-N/2,N/2)/(N*pixelSize), int(N/2))
@@ -546,8 +603,8 @@ def ewpc_lambda(Q_Nx,Q_Ny):
 # def make_Fourier_coords2D(Nx, Ny, pixelSize=1):
 #    """
 #    Generates Fourier coordinates for a (Nx,Ny)-shaped 2D array.
-#	Specifying the pixelSize argument sets a unit size.
-#	"""
+#   Specifying the pixelSize argument sets a unit size.
+#   """
 #    qx = make_Fourier_coords1D(Nx,pixelSize)
 #    qy = make_Fourier_coords1D(Ny,pixelSize)
 #    qy,qx = np.meshgrid(qy,qx)
