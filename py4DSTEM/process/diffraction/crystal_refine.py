1646574ed29d059387b9b172d14c2b2c9f652828
SE Zeltmann <steven.zeltmann@lbl.gov>
2022-02-17 13:14:27 -0800

    starting to implement thickness matching

--- py4DSTEM/process/diffraction/crystal_refine.py
+++ py4DSTEM/process/diffraction/crystal_refine.py
@@ -8,7 +8,13 @@ from ...io.datastructure import PointList
 
 
 def estimate_thickness(
-    self, bragg_peaks: PointList, bloch_beams: PointList, thickness: np.ndarray
+    self, 
+    bragg_peaks: PointList, 
+    orientation: np.ndarray, 
+    bloch_beams: PointList, 
+    thickness: np.ndarray,
+    min_peaks: int = 4,
+    ax=None,
 ) -> float:
     """
     Estimate thickness of diffraction pattern encoded in ``bragg_peaks`` by computing
@@ -21,7 +27,56 @@ def estimate_thickness(
                                         calculation
         thickness (ndarray):            Array of thickness values to compare against
     """
-    return
+    
+    if np.atleast_1d(bragg_peaks.data).shape[0] < min_peaks:
+        return 0.
+    
+    ZA = orientation[:,2] # this should be the ZA component of the orientation matrix ??
+    
+    bloch = self.generate_dynamical_diffraction_pattern(bloch_beams,
+                                                        thickness=thickness,
+                                                        zone_axis=ZA,
+                                                        always_return_list=True,
+                                                       )
+    
+    # normalize each Bloch wave pattern to the direct beam intensity
+    zerobeam = [0, 0, 0]
+    pld = bloch[0].data
+    idx = np.argwhere(
+        np.atleast_1d(np.logical_and(
+            np.logical_and(pld["h"] == zerobeam[0], pld["k"] == zerobeam[1]),
+            pld["l"] == zerobeam[2],
+        ))
+    )[0][0]
+    for b in bloch:
+        b.data['intensity'] /= b.data['intensity'][idx]
+    
+    # get indices that match beams in bragg_peaks to beams in bloch_beams
+    hkl_bragg = np.vstack((bragg_peaks.data['h'],bragg_peaks.data['k'],bragg_peaks.data['l'])).T
+    hkl_bloch = np.vstack((bloch_beams.data['h'],bloch_beams.data['k'],bloch_beams.data['l'])).T
+    a,b = np.mgrid[0:hkl_bragg.shape[0],0:hkl_bloch.shape[0]]
+    # matches contains two arrays, one with the incdices into bragg_beams and one with indices into bloch_beams,
+    # which correspond to slices that pair up the common beams correctly
+    matches = np.nonzero(np.all((hkl_bragg[a.ravel(),:] == hkl_bloch[b.ravel(),:]).reshape(a.shape + (3,)),axis=2))
+    
+    # cost function: takes two structures arrays with (qx,qy,I,h,k,l) and returns a floating point number for the score
+    def cost_function(bps, bbs):
+        return np.sum(  np.sqrt((bps['intensity'] - np.sqrt(bbs['intensity']))**2) * np.sqrt(bps['intensity']))
+        # return (np.sum( bps['intensity'] * bbs['intensity'] ) - 1) / (np.sum(bbs['intensity']) - 1)
+        # return np.sum( bps['intensity'] * np.sqrt(bbs['intensity'] )) / (np.sum(np.sqrt(bbs['intensity'])))
+    
+    scores = np.array([cost_function(bragg_peaks.data[matches[0]], bbs.data[matches[1]]) for bbs in bloch])
+    
+    # if ax is not None:
+    #     ax[0].plot(thickness,scores)
+    #     bt = np.vstack([bb.data[matches[1]]['intensity'] for bb in bloch]).T
+    #     for b in bt:
+    #         ax[1].plot(thickness,np.sqrt(b))
+    #     for b,c in zip(bragg_peaks.data[matches[0]]['intensity'],plt.rcParams['axes.prop_cycle']):
+    #         ax[1].axhline(b,c=c['color'])
+    
+    
+    return thickness[np.nanargmin(scores)]
 
 
 def index_Bragg_peaks_from_orientation(
@@ -107,4 +162,4 @@ def index_Bragg_peaks_from_orientation(
                 )
             )
 
-    return PointList(match_dtype, np.squeeze(np.array(matches)))
+    return PointList(match_dtype, np.squeeze(np.array(matches))), sim_peaks
